61d4bc7b16951b3da2e9a62093079322f3e8b1f1
DrErvin <ervin.dragovic@gmail.com>
2025-02-04 14:04:34 +0100

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,6 @@
 // export const API_URL = 'https://www.arbeitnow.com/api/job-board-api';
 // export const API_URL = 'http://127.0.0.1:8080/server/data.json';
-export const API_URL = 'http://localhost:3000';
+export const API_URL = 'http://localhost:3000'; // Local backend
 export const UNIVERSITY_API_URL = 'http://universities.hipolabs.com';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
